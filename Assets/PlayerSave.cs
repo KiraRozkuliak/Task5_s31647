6ff907e531987f485a00d742fdd301c25f31b0a9
KiraRozkuliak <s31647@pjwstk.edu.pl>
2025-12-09 18:47:07 +0100

    9/12

---
+++ Assets/PlayerSave.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+// Unity Script 10 references
+public class PlayerSave : MonoBehaviour
+{
+    // przykładowe dane do zapisania
+    public int health = 100;
+    public int coins = 0;
+
+    // Unity Message 10 references
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Save();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    // 0 reference
+    public void Save()
+    {
+    SaveData data = new SaveData(); // Tworzenie obiektu do zapisu danych
+    data.health = health; // Zapisuje zdrowie
+    data.coins = coins; // Zapisuje liczbę monet
+    data.position = new float[3] // Zapisuje pozycję gracza
+    {
+        transform.position.x,
+        transform.position.y,
+        transform.position.z
+    };
+    // Zapisuje dane za pomocą SaveSystem
+    SaveSystem.Save(data);
+    Debug.Log("Game Saved");
+    }
+
+    public void Load()
+    {
+        // Wczytuje dane za pomocą SaveSystem
+        SaveData data = SaveSystem.Load();
+        // Jeśli nie ma danych, wyświetla ostrzeżenie i zwraca nic
+        if (data == null)
+        {
+            Debug.LogWarning("No save data found");
+            return;
+        }
+        health = data.health; // Wczytuje zdrowie
+        coins = data.coins; // Wczytuje liczbę monet
+                            // Wczytuje pozycję gracza
+        transform.position = new Vector3(
+            data.position[0],
+            data.position[1],
+            data.position[2]
+        );
+        Debug.Log("Game Loaded");
+    }
+
+}
---
+++ Assets/SaveData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public float[] position; // pozycja gracza
+    public int health; // zdrowie gracza
+    public int coins; // liczba monet
+}
